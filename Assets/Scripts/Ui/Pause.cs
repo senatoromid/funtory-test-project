5de0ae9f13aef49c6160ee698ba9efb0ccfa2f67
Omid <59394589+senatoromid@users.noreply.github.com>
2022-12-11 16:32:03 +0330

    طراحی رابط کاربری  (#2)
    fix bug and create new ui and new game play

---
+++ Assets/Scripts/Ui/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public Transform box;
+    public CanvasGroup back;
+
+    private void OnEnable()
+    {
+       
+        back.alpha = 0;
+        back.LeanAlpha(1, .4f);
+        box.localPosition = new Vector2(0, -Screen.height);
+        box.LeanMoveLocalY(0, .4f).setEaseInOutExpo().delay = .05f;
+        StartCoroutine(PauseGame());
+    }
+    public void CloseDialog()
+    {
+        back.LeanAlpha(0, .5f);
+        box.LeanMoveLocalY(-Screen.height, .4f).setEaseInExpo().setOnComplete(OnCompelet);
+        Time.timeScale = 1;
+    }
+    void OnCompelet()
+    {
+        gameObject.SetActive(false);
+    }
+    IEnumerator PauseGame()
+    {
+        yield return new WaitForSeconds(.5f);
+        Time.timeScale = 0;
+    }
+
+}
